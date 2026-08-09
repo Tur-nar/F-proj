936e605488c0a0bd73a1aa351d014239ad9cd8bc
Tur-nar <timmyturnerrr50@gmail.com>
2026-08-09 19:37:03 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,135 @@
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+import pandas as pd
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+# Load your trained model with joblib
+model = joblib.load('model/heart_model.joblib')
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+
+
+class RegisterForm(FlaskForm):
+    email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)  # Removed the remember parameter
+            flash('Logged in successfully!', 'success')
+            return redirect(url_for('dashboard'))
+        flash('Invalid username or password', 'danger')
+    return render_template('login.html', form=form)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='pbkdf2:sha256')
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('You have successfully registered!', 'success')
+        return redirect(url_for('login'))
+    return render_template('signup.html', form=form)
+    
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+   
+
+
+@app.route("/dashboard")
+@login_required
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/help")
+@login_required
+def helps():
+    return render_template("help.html")
+
+
+@app.route("/heart")
+@login_required
+def heart():
+    return render_template("heart.html")
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/predictheart', methods=['POST'])
+def predictheart():
+    input_features = [float(x) for x in request.form.values()]
+    features_value = [np.array(input_features)]
+
+    features_name = ["age", "sex", "cp", "trestbps", "chol", "fbs", "restecg",
+                     "thalach", "exang", "oldpeak", "slope", "ca", "thal",]
+
+    df = pd.DataFrame(features_value, columns=features_name)
+    output = model.predict(df)
+
+    if output == 1:
+        res_val = "a high risk of Heart Disease"
+    else:
+        res_val = "a low risk of Heart Disease"
+
+    return render_template('heart_result.html', prediction_text='Patient has {}'.format(res_val))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
